333548672c1faaf81b060949264dc8d2b2c95e9b
CristianOrizano <cristianorizano57@gmail.com>
2023-06-30 00:51:48 -0500

    sin bot

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -9,19 +9,6 @@ export class InicioComponent implements  AfterViewInit {
   showWatsonChat = true;
 
   ngAfterViewInit() {
-    if (this.showWatsonChat) {
-    // Coloca tu código de script aquí
-    (window as any).watsonAssistantChatOptions = {
-      integrationID: "735b330d-d6ef-4606-826a-a98549702925", // The ID of this integration.
-      region: "au-syd", // The region your integration is hosted in.
-      serviceInstanceID: "99f16dc1-95c6-41ad-9c90-77783643eb94", // The ID of your service instance.
-      onLoad: function(instance: { render: () => void; }) { instance.render(); }
-    };
-    setTimeout(function(){
-      const t=document.createElement('script');
-      t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + ((window as any).watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
-      document.head.appendChild(t);
-    });
-  }
+ 
  }
 }
